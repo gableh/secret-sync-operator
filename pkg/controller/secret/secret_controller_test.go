047c37a2cacefdde16b3d38aca440498ef7a842c
Gable Heng <gableh@thoughtworks.com>
2020-01-24 10:12:50 +0800

    Refactor Test

--- pkg/controller/secret/secret_controller_test.go
+++ pkg/controller/secret/secret_controller_test.go
@@ -3,9 +3,8 @@ package secret
 import (
 	"context"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	zap "sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"testing"
-
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -17,88 +16,91 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-func TestSecretControllerShouldUpdateSecrets(t *testing.T) {
-	logf.SetLogger(zap.Logger(true))
-	secret := &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test-secret",
-			Namespace: "default",
-			Annotations: map[string]string{
-				"hash": "",
-			},
-			Labels: map[string]string{
-				"sso.gable.dev/secret": "true",
-			},
+var secret = &corev1.Secret{
+	ObjectMeta: metav1.ObjectMeta{
+		Name:      "test-secret",
+		Namespace: "default",
+		Annotations: map[string]string{
+			"hash": "",
 		},
-		Data: map[string][]byte{
-			"test": []byte("aW5pdGlhbA=="),
+		Labels: map[string]string{
+			"sso.gable.dev/secret": "true",
 		},
-	}
+	},
+	Data: map[string][]byte{
+		"test": []byte("aW5pdGlhbA=="),
+	},
+}
 
-	deployment := &appsv1.Deployment{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Deployment",
-			APIVersion: "apps/v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test-deployment",
-			Namespace: "default",
-			Labels: map[string]string{
-				"sso.gable.dev/secret": "test-secret",
-				"updatedSecretAt":      "0",
+var testPodSpec = corev1.PodSpec{
+	Containers: []corev1.Container{
+		corev1.Container{
+			Name:  "test-container",
+			Image: "busybox",
+			Command: []string{
+				"sh",
+				"-c",
+				"echo Hello Kubernetes! && sleep 3600",
 			},
-		},
-		Spec: appsv1.DeploymentSpec{
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"app": "myapp",
-				},
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "test-pod",
-					Namespace: "default",
-					Labels: map[string]string{
-						"app":             "my-app",
-						"updatedSecretAt": "0",
-					},
-				},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						corev1.Container{
-							Name:  "test-container",
-							Image: "busybox",
-							Command: []string{
-								"sh",
-								"-c",
-								"echo Hello Kubernetes! && sleep 3600",
+			Env: []corev1.EnvVar{
+				corev1.EnvVar{
+					Name: "secret-value",
+					ValueFrom: &corev1.EnvVarSource{
+						SecretKeyRef: &corev1.SecretKeySelector{
+							LocalObjectReference: corev1.LocalObjectReference{
+								Name: "test-secret",
 							},
-							Env: []corev1.EnvVar{
-								corev1.EnvVar{
-									Name: "secret-value",
-									ValueFrom: &corev1.EnvVarSource{
-										SecretKeyRef: &corev1.SecretKeySelector{
-											LocalObjectReference: corev1.LocalObjectReference{
-												Name: "test-secret",
-											},
-											Key: "test",
-										},
-									},
-								},
-							},
+							Key: "test",
 						},
 					},
 				},
 			},
 		},
-	}
+	},
+}
+
+var deployment = &appsv1.Deployment{
+	TypeMeta: metav1.TypeMeta{
+		Kind:       "Deployment",
+		APIVersion: "apps/v1",
+	},
+	ObjectMeta: metav1.ObjectMeta{
+		Name:      "test-deployment",
+		Namespace: "default",
+		Labels: map[string]string{
+			"sso.gable.dev/secret": "test-secret",
+			"updatedSecretAt":      "0",
+		},
+	},
+	Spec: appsv1.DeploymentSpec{
+		Selector: &metav1.LabelSelector{
+			MatchLabels: map[string]string{
+				"app": "myapp",
+			},
+		},
+		Template: corev1.PodTemplateSpec{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-pod",
+				Namespace: "default",
+				Labels: map[string]string{
+					"app":             "my-app",
+					"updatedSecretAt": "0",
+				},
+			},
+			Spec: testPodSpec,
+		},
+	},
+}
+// @TODO annotations hash, updatedSecretAt will currently fail if not set before hand, is there a way around this?
+func TestSecretControllerShouldUpdateSecrets(t *testing.T) {
+	logf.SetLogger(zap.Logger(true))
 
 	objs := []runtime.Object{secret, deployment}
 
 	s := scheme.Scheme
 	s.AddKnownTypes(appsv1.SchemeGroupVersion, deployment)
 	s.AddKnownTypes(corev1.SchemeGroupVersion, secret)
-	cl := fake.NewFakeClient(objs...)
+	cl := fake.NewFakeClientWithScheme(s, objs...)
 
 	opt := client.MatchingLabels(map[string]string{"sso.gable.dev/secret": "test-secret"})
 	secretList := &corev1.SecretList{}
